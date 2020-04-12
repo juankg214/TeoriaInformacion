f107c37272840c0fadb87a6bfba00fcc5b65d358
Juan Camilo Gomez Lopez <juankg214@gmail.com>
2020-04-12 18:47:23 -0500

    Plotting

--- Lab1/Punto1.m
+++ Lab1/Punto1.m
@@ -19,8 +19,8 @@ function l = Detect_number_of_integrals(x)
   endfor
   l(end+1) = contador;
 endfunction
-k=10; 
-y=[-1 1];
+k=[1 5 10 20]; 
+y=[-1 1 1 -1 1];
 T0 = length(y);
 l = Detect_number_of_integrals(y);
 a0 = 0;
@@ -35,46 +35,53 @@ for i=1:length(l)
   a0 = a0 + 2/T0.*quad(f,-T0./2.+r,-T0./2.+w);
   r = w;
 endfor
-f = "";
+f = [" ";" ";" ";" "];
+f = cellstr(f)
 an = 0;
 bn = 0;
 sum=0;
 r=0;
 w=0;
-for j=1:k
-    an = 0;
-    bn = 0;
-    sum=0;
-    r=0;
-    w=0;
-  for i=1:length(l)
-    sum = sum .+ l(i);
-    f1 = @(x) y(sum)*cos(j*2*pi/T0*x);
-    f2 = @(x) y(sum)*sin(j*2*pi/T0*x);
-    w = w .+ (l(i)./length(y)).*T0;
-    an = an + 2/T0*quad(f1,-T0./2.+r,-T0./2.+w);
-    bn = bn + 2/T0*quad(f2,-T0./2.+r,-T0./2.+w);
-    r = w;
+for z=1:length(k)
+  for j=1:k(z)
+      an = 0;
+      bn = 0;
+      sum=0;
+      r=0;
+      w=0;
+    for i=1:length(l)
+      sum = sum .+ l(i);
+      f1 = @(x) y(sum)*cos(j*2*pi/T0*x);
+      f2 = @(x) y(sum)*sin(j*2*pi/T0*x);
+      w = w .+ (l(i)./length(y)).*T0;
+      an = an + 2/T0*quad(f1,-T0./2.+r,-T0./2.+w);
+      bn = bn + 2/T0*quad(f2,-T0./2.+r,-T0./2.+w);
+      r = w;
+    endfor
+    #disp(bn);
+   aux_car = "+";
+   if(j==k(z))
+    aux_car = "";
+   endif 
+    #disp(an);
+    f(z) =  strcat(f(z),num2str(an),"*cos(", num2str(j*2*pi/T0),"*x)+", num2str(bn) ,"*sin(", num2str(j*2*pi/T0),"*x)",aux_car);
   endfor
-  disp(bn);
- aux_car = "+"
- if(j==k)
-  aux_car = ""
- endif 
-
-  f =  strcat(f,num2str(an),"*cos(", num2str(j*2*pi/T0),"*x)+", num2str(bn) ,"*sin(", num2str(j*2*pi/T0),"*x)",aux_car);
-
 endfor
-  
-f=inline(strcat(f,"+",num2str(a0/2)),"x")
-
-u = [-T0/2:T0/2];
-fplot(f);
-legend("hide");
-hold on;
-
-y_periodic = y'*ones(1,length(y));
-y_periodic = (y_periodic(:))';
-stem(u,y_periodic(1:length(u)), "linewidth",3)
+function plot_k(i,v,y,T0,k,a0)
+  u = [-T0/2:T0/2];
+  y_periodic = y'*ones(1,length(y));
+  y_periodic = (y_periodic(:))';
+  subplot (2, 2, i)
+  hold on;
+  title(sprintf("Serie de Fourier con K = %i",k))
+  fplot(inline(strcat(v,"+",num2str(a0/2)),"x"))
+  stem(u,y_periodic(1:length(u)), "linewidth",3)
+  legend("hide");
+  hold off;
+endfunction
+for i=1:length(k)
+  plot_k(i,char(f(i)),y,T0,k(i),a0)
+endfor
 axis("auto")
+
 
