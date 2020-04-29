e1f591545d3e174fed6a503779af3e03868c4916
Juan Camilo <jcgomezlo@unal.edu.co>
2020-04-29 11:38:28 -0500

    lab 2 comments

--- Lab2/punto1.m
+++ Lab2/punto1.m
@@ -1,51 +1,78 @@
-#Definición de los tiempos
-lim1 = -3; lim2 = 3;      % Para dibujar la señal entre lim1 y lim2
-At = .01;                 % Espacio entre tiempos
-t = [lim1:At:lim2];       % Vector de tiempos
+close all
+clc
 
-#Señal a enviar
-m = 2*sin(t) + sin(2*t);    % Definición de la señal m(t)
-A = max(m);                 % Amplitud máxima de la señal
-fm = 2;                     % Frecuencia máxima de la señaal
+#Definición de los tiempos y Señal
+lim1 = -pi;                           %Limites 
+lim2 = pi;      
+At = .01;                 
+t = [lim1:At:lim2];                   %Tiempos en los limites   
+fm = 2;                               %Frecuencia Maxima
+m =  sin(2*pi*t) + 2*sin(2*pi*fm*t);  %Señal
 
-
-#------------ MUESTREO ------------
-
-#Cálculo del tiempo entre muestras
-Ts = 1/(2*fm);            % Tiempo muestral según Teorema del Muestreo
-d_Ts = [lim1:Ts:lim2];    % Vector de tiempos de muestreo
-p = Ts/At;                % Espacio entre una muestra y la siguiente
-
-#Definición de la señal muestreada m_s(t)
-m_s = ones(1,length(d_Ts));
-for i = 0:length(m_s)-1
-  m_s(i+1) = m((i*p)+1);
-endfor
-
-#Gráfica de las señales m(t) y m_s(t)
 figure(1);
-subplot(3,1,1); plot(t,m,'b'); grid; title('Señal original m(t)');
-subplot(3,1,2); stem(d_Ts,m_s,'b'); grid; title('Señal Muestreada m_S(t)');
+#Señal Original Grafica
+subplot(4,1,1); 
+plot(t,m,'b');
+grid; 
+title('Señal original m(t)');
+xlabel("t"),ylabel("m(t)");                               
+  
 
+#------------------- Muestreo -------------------------------
+Ts = 1/(2*fm);                        %Tiempo muestral 
+d_Ts = [lim1:Ts:lim2];                %Vector de tiempos para muestreo
+p = Ts/At;                            %Espacio entre una muestra y la siguiente
 
-#---------- CUANTIZACIóN ----------
-
-#Definición de los niveles de cuantización
-n = 2;                % Número de bits por muestra
-L = 2^n;              % Número de niveles
-w = 2*A/L;            % Ancho (width) de cada nivel
-
-#Cálculo de la señal cuantizada
-fr = zeros(1,length(m_s));    % Franjas en las que cae cada muestra
-q = zeros(1,length(m_s));     % Señal cuantizada
-for j = 1:length(m_s);
-  fr(j) = floor((m_s(j)-A)/w);
-  q(j) = A + w/2 + fr(j)*w;
+ms = ones(1,length(d_Ts));
+for i = 0:length(ms)-1
+  ms(i+1) = m((i*p)+1);               %Se escogen los puntos de la señal original en cada Tiempo muestral
 endfor
-fr = (L.+fr);
 
-#Gráfica de las señales muestreada y cuantizada
-subplot(3,1,3); stem(d_Ts, q,'b'); grid; title(['Señal Cuantizada en ' num2str(L) ' niveles']);
+
+#Señal Muestrada
+subplot(4,1,2);
+hold on;
+stem(d_Ts,ms,'b'); 
+plot(t,m,'b');
+grid; 
+title('Señal Muestreada ms(t)');
+xlabel("t"),ylabel("m(t)");
+hold off;
+
+#---------- Cuantificación ----------
+
+mp = max(m);           % Amplitud maxima. En nuestro ejemplo 2.7358
+n = 2;                 % Número de bits por muestra
+L = 2^n;               % Número de niveles
+w = 2*mp/L;            % Ancho (width) de cada nivel. En nuestro ejemplo ejemplo 1.3679
+
+l_ms = length(ms);     % Numero de puntos de muestreo
+fr = zeros(1,l_ms);    % Franjas en las que cae cada muestra, Valores de fr(j) = -4:-1
+q = zeros(1,l_ms);     % Señal Cuantificada en cada franja, cada franja tendra un valor de -w-w/2, -w+w/2, w+w/2, w-w/2
+for j = 1:l_ms;
+  fr(j) = floor((ms(j)-mp)/w);
+  q(j) = mp + w/2 + fr(j)*w
+endfor
+fr = (L.+fr)            % Toma valores de 0:3
+
+#Señal Cuantificada
+subplot(4,1,3); 
+hold on;
+plot(t,m,'b');
+stem(d_Ts, q,'b'); 
+grid; 
+title(['Señal Cuantizada en ' num2str(L) ' niveles']);
+hold off;
+#------------------- demodulacion -------------------------------
+dem = ones(1,length(d_Ts));
+for i = 0:length(ms)-2
+  dem(i+1) = (ms(i+1)-ms(i+2)/2);            %Se escogen los puntos de la señal original en cada Tiempo muestral
+endfor
+subplot(4,1,4); 
+stem(d_Ts,dem,'b');
+grid; 
+title('Señal original m(t)');
+xlabel("t"),ylabel("m(t)"); 
 
 
 #---------- CODIFICACÓN ----------
@@ -56,7 +83,6 @@ cod = '';
 for j = 1:length(fr)
   cod = strcat(cod,dec2bin(fr(j),n));
 endfor
-
 #Creación de un vector de bits a partir del string binario 
 bits = ones(1,length(cod));
 for i = 1:length(cod)
